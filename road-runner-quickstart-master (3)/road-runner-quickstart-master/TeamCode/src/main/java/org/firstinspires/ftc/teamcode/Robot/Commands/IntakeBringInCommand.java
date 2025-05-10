62653f419d64fa9d61e9c5c91d815a90f8a475ab
RS23443 <23443.rs@gmail.com>
2025-05-10 14:29:29 -0500

    Here is the beginning of the code for the Indiana Premier Event for the 2024-2025 Season - 23443 RoboSupport

--- road-runner-quickstart-master (3)/road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Commands/IntakeBringInCommand.java
+++ road-runner-quickstart-master (3)/road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Commands/IntakeBringInCommand.java
@@ -9,7 +9,7 @@ public class IntakeBringInCommand extends CommandBase {
         this.intake = intake;
         addRequirements(intake);
     }
-
+//here is a comment that has no purpose
     @Override
     public void initialize() {
         intake.bringinintake();
